9d8416d94bdc4e6e9b5ed572c92a95ecce160994
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2015-07-17 13:58:08 +0900

    tests(parse_config): improve coverage

--- zktraffic/tests/test_parse_config.py
+++ zktraffic/tests/test_parse_config.py
@@ -25,9 +25,13 @@ class ParseConfigTestCase(unittest.TestCase):
       'server.1=localhost:2780:2783:participant;0.0.0.0:2181\n',
       'server.2=localhost:2781:2784:participant;0.0.0.0:2182\n',
       'server.3=localhost:2782:2785:participant;0.0.0.0:2183\n',
-      'version=0\n'))
+      'version=0\n',
+      '\n',
+      'unsupported_key=unsupported_value\n'
+    ))
     config = QuorumConfig(config_string)
     l = config.entries
+    self.assertIsInstance(l[0], QuorumConfig.Server)
     self.assertEqual(l[0].sid, 1)
     self.assertEqual(l[0].zab_fle_hostname, 'localhost')
     self.assertEqual(l[0].zab_port, 2780)
@@ -35,12 +39,39 @@ class ParseConfigTestCase(unittest.TestCase):
     self.assertEqual(l[0].learner_type, 'participant')
     self.assertEqual(l[0].zk_hostname, '0.0.0.0')
     self.assertEqual(l[0].zk_port, 2181)
+
+    self.assertIsInstance(l[1], QuorumConfig.Server)
+    self.assertEqual(l[1].sid, 2)
+
+    self.assertIsInstance(l[2], QuorumConfig.Server)
+    self.assertEqual(l[2].sid, 3)
+
+    self.assertIsInstance(l[3], QuorumConfig.Version)
     self.assertEqual(l[3].version, 0)
 
-  def test_parse_bad_config(self):
+    self.assertIsInstance(l[4], QuorumConfig.Unsupported)
+
+  def test_parse_config_with_bad_server(self):
     config_string = ''.join((
       'server.1=localhost:2780:2783:participant;0.0.0.0:2181\n',
       'server.2=localhost:2781\n',
+      'server.3=localhost:2782:2785:participant;0.0.0.0:2183\n',
+      'version=0\n'))
+    self.assertRaises(QuorumConfig.BadConfig, QuorumConfig, config_string)
+
+
+  def test_parse_config_with_bad_client(self):
+    config_string = ''.join((
+      'server.1=localhost:2780:2783:participant;0.0.0.0:2181\n',
+      'server.2=localhost:2781:2784:participant;0.0.0.0:2182:2192\n',
+      'server.3=localhost:2782:2785:participant;0.0.0.0:2183\n',
+      'version=0\n'))
+    self.assertRaises(QuorumConfig.BadConfig, QuorumConfig, config_string)
+
+  def test_parse_config_with_bad_learner(self):
+    config_string = ''.join((
+      'server.1=localhost:2780:2783:participant;0.0.0.0:2181\n',
+      'server.2=localhost:2781:2784:foobar;0.0.0.0:2182\n',
       'server.3=localhost:2782:2785:participant;0.0.0.0:2183\n',
       'version=0\n'))
     self.assertRaises(QuorumConfig.BadConfig, QuorumConfig, config_string)
